02acacc1af2915cfe7b0bf4233402ae990addc7f
jinpei0908 <jinpei0908@gmail.com>
2021-07-11 19:08:16 +0900

    Add 8th step files

--- src/01/os/main.c
+++ src/01/os/main.c
@@ -1,49 +1,22 @@
 #include "defines.h"
 #include "interrupt.h"
-#include "intr.h"
+#include "kozos.h"
 #include "lib.h"
-#include "serial.h"
 
-static void intr(softvec_type_t type, unsigned long sp) {
-  int c;
-  static char buf[32];
-  static int len;
-
-  c = getc();
-
-  if (c != '\n') {
-    buf[len++] = c;
-  } else {
-    buf[len++] = '\0';
-    if (!strncmp(buf, "echo", 4)) {
-      puts(buf + 4);
-      puts("\n");
-    } else {
-      puts("unknown.\n");
-    }
-    puts("> ");
-    len = 0;
-  }
+/* システムタスクとユーザスレッドの起動 */
+static int start_threads(int argc, char *argv[]) {
+  kz_run(test08_1_main, "command", 0x100, 0, NULL);
+  return 0;
 }
 
 int main(void) {
-  /* 割り込み無効にする */
-  INTR_DISABLE;
+  INTR_DISABLE; /* 割り込み無効にする */
 
   puts("kozos boot succeed!\n");
 
-  softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
-  serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
-
-  puts("> ");
-
-  /* 割り込み有効にする */
-  INTR_ENABLE;
-
-  while (1) {
-    /* 省電力モードに移行 */
-    asm volatile("sleep");
-  }
+  /* OSの動作開始 */
+  kz_start(start_threads, "start", 0x100, 0, NULL);
+  /* ここには戻ってこない */
 
   return 0;
 }
