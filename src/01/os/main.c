1d8c18c82e88d5a0f1f3cba628fb522069373f42
jinpei0908 <jinpei0908@gmail.com>
2021-09-20 11:41:00 +0900

    Add 11th step files

--- src/01/os/main.c
+++ src/01/os/main.c
@@ -5,7 +5,8 @@
 
 /* システムタスクとユーザスレッドの起動 */
 static int start_threads(int argc, char *argv[]) {
-  kz_run(test10_1_main, "test_10_1", 1, 0x100, 0, NULL);
+  kz_run(test11_1_main, "test_11_1", 1, 0x100, 0, NULL);
+  kz_run(test11_2_main, "test_11_2", 2, 0x100, 0, NULL);
 
   kz_chpri(15); /* 優先順位を下げてアイドルスレッドに移行する */
   INTR_ENABLE;
